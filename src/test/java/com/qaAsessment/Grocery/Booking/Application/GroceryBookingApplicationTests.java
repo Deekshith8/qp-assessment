66ad78ebc1accafe24d5198aeaae1142b7269468
Deekshith8 <deekshith.1rn17ei010@gmail.com>
2024-10-30 16:39:54 +0530

    Started the project

---
+++ src/test/java/com/qaAsessment/Grocery/Booking/Application/GroceryBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qaAsessment.Grocery.Booking.Application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroceryBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
